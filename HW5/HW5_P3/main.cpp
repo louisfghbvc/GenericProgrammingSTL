b22fbb35e96c96e4adaf1c06d2029f4652876e66
andyet04 <andyet04@yahoo.com.tw>
2020-05-30 19:15:47 +0800

    第一次修改

--- HW5/HW5_P3/main.cpp
+++ HW5/HW5_P3/main.cpp
@@ -7,23 +7,13 @@ int main(int argc,char *argv[])
 {
     string A;
     smatch m;
-    regex e1 ("My phone number is \\+\\(([0-9]{3})\\)\\-(\\d)\\-([0-9]{4})\\-([0-9]{4})\\.");
-    regex e2 ("Yours is \\+\\(([0-9]{3})\\)\\-(\\d)\\-([0-9]{4})\\-([0-9]{4}) ");
-    regex e3 ("Today is a sunny day and I will call the number \\+\\(([0-9]{3})\\)\\-(\\d)\\-([0-9]{4})\\-([0-9]{4})\\.");
+    regex e1 ("(.*)\\+\\(([0-9]{3})\\)\\-([0-9]{1})\\-\\3([0-9]{3})\\-([0-9]{4})(.*)");
     while(!cin.eof())
     {
         getline(cin,A);
         if(regex_match (A, m,e1))
         {
-            A=regex_replace(A, e1,"My phone number is +(XXX)-X-XXXX-$4.");
-        }
-        else if(regex_match (A, m,e2))
-        {
-            A=regex_replace(A, e2,"Yours is +(XXX)-X-XXXX-$4.");
-        }
-        else if(regex_match (A, m,e3))
-        {
-            A=regex_replace(A, e3,"Today is a sunny day and I will call the number +(XXX)-X-XXXX-$4.");
+            A=regex_replace(A, e1,"$1+(XXX)-X-XXXX-$5.");
         }
         cout<<A<<endl;
     }
@@ -39,3 +29,15 @@ Today is a sunny day and I will call the number +(123)-1-1123-3455.
 +(aaa)-b-b123-3388
 Goodbye!
 */
+
+
+
+
+
+
+
+
+
+
+
+
