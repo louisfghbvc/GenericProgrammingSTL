e092b2d6aa3fa66d562e355b90425cd200cc5476
andyet04 <andyet04@yahoo.com.tw>
2020-06-16 17:58:27 +0800

    2020/6/16 修改

--- HW4 應用程式設計/data generate/main.cpp
+++ HW4 應用程式設計/data generate/main.cpp
@@ -8,29 +8,42 @@
 using namespace std;
 
 const int mod=1e9;
-int main(){
-    int N, M, i, j;
+int main (int argc, char* argv[]){//argv[0] English_name argv[1] group argv[2] length
+    int i, j;
     srand( time( NULL ) );
-    int A[5]={1000, 5000, 10000, 50000, 100000},B[3]={100, 500, 1000};
-    for(int k=0;k<5;k++)
+    char alphbet[52];
+    alphbet[0]='a';
+    for(i=0;i<26;i++)
+        alphbet[i+1]=alphbet[i]+1;
+    alphbet[26]='A';
+    for(i=26;i<52;i++)
+        alphbet[i+1]=alphbet[i]+1;
+    if(argc!=1)
     {
-    	for(int l=0;l<3;l++)
-    	{
-            N=A[k];
-    		M=B[l];
-    	    string name= "N"+to_string(N)+"M"+to_string(M);
-            ofstream out (name+".in");
-
-			for( i = 0; i < N; i++ ){
-				for( j = 0; j < M; j++ ){
-					out << rand()% mod << " ";
-				}
-                out << endl;
-			}
-            out << 0 << " " << 0 << "\n";
-            out.close();
-    	}
+        int a=atoi(argv[2]),b=atoi(argv[3]),English_name=atoi(argv[1]);
+        string name= "N"+to_string(a)+"M"+to_string(b);
+        cout<<English_name<<" "<<a<<" "<<b<<endl;
+        ofstream out (name+".in");
+        for(int k=0;k<a;k++)
+        {
+            for( j = 0; j < English_name; j++ ){
+                out << alphbet[rand()% 52];
+            }
+            out<<" ";
+            for( j = 0; j < b; j++ ){
+                out << rand()% mod << " ";
+            }
+            out << endl;
+        }
+        out << 0 << " " << 0 << "\n";
+        out.close();
     }
     cout<<"end"<<endl;
     return 0;
 }
+
+
+
+
+
+
